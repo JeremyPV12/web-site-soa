7f83c748af82d87603127659076628609f584afd
Harol Avelino Delzo <harolsd121@gmail.com>
2025-08-25 21:50:56 -0500

    Structure proyect

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { MainPageClientComponent } from './features/client/main-page-client/main-page-client.component';
+import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: 'clientes', component: MainPageClientComponent },
+      { path: '', redirectTo: 'clientes', pathMatch: 'full' }
+    ]
+  },
+//   { path: 'login', component: LoginComponent }
+];
+
---
+++ src/app/features/client/main-page-client/main-page-client.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-page-client',
+  imports: [],
+  templateUrl: './main-page-client.component.html',
+  styleUrl: './main-page-client.component.css'
+})
+export class MainPageClientComponent {
+
+}
